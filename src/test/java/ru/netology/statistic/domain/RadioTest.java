6e856941f83d9c9d9841983598e1e79632b6a2aa
TatyanaMilyutkina <milytkinata@gmail.com>
2022-06-11 23:32:33 +0400

    New Resuece for Radio

--- src/test/java/ru/netology/statistic/domain/RadioTest.java
+++ src/test/java/ru/netology/statistic/domain/RadioTest.java
@@ -10,6 +10,60 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RadioTest {
 
     @ParameterizedTest
+    @CsvFileSource(resources = {"/prevStation.csv"})
+    public void prevParamRadioStation(int station, int expected) {
+        Radio prev = new Radio();
+        prev.prevStation(station);
+        int actual = prev.prevStation(station);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/prevCountStation.csv"})
+    public void prevCountParamRadioStation(int station, int expected, int maxCountStation) {
+        Radio prev = new Radio(maxCountStation);
+        prev.prevStation(station);
+        int actual = prev.prevStation(station);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/numStation.csv"})
+    public void numParamRadioStation(int station, int expected) {
+        Radio num = new Radio();
+        num.setRadioStation(station);
+        int actual = num.getRadioStation();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/numCountStation.csv"})
+    public void numCountParamRadioStation(int station, int expected, int maxCountStation) {
+        Radio numCount = new Radio(maxCountStation);
+        numCount.setRadioStation(station);
+        int actual = numCount.getRadioStation();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/nextStation.csv"})
+    public void nextParamRadioStation(int station, int expected) {
+        Radio next = new Radio();
+        next.nextStation(station);
+        int actual = next.nextStation(station);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/nextCountStation.csv"})
+    public void nextCountParamRadioStation(int station, int expected, int maxCountStation) {
+        Radio next = new Radio(maxCountStation);
+        next.nextStation(station);
+        int actual = next.nextStation(station);
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
     @CsvFileSource(resources = {"/prevVolume.csv"})
     public void prevParamRadioVolume(int volume, int expected) {
         Radio prev = new Radio();
@@ -19,240 +73,20 @@ public class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = {"/prevStation.csv"})
-    public void prevParamRadioStation(int station, int expected) {
-        Radio prev = new Radio();
-        prev.prevStation(station);
-        int actual = prev.prevStation(station);
+    @CsvFileSource(resources = {"/numVolume.csv"})
+    public void numParamRadioVolume(int volume, int expected) {
+        Radio num = new Radio();
+        num.setVolumeStation(volume);
+        int actual = num.getVolumeStation();
         assertEquals(expected, actual);
     }
 
-    @Test
-    void normalParamRadio() {
-        Radio station = new Radio();
-
-        station.setRadioStation(3);
-        station.setVolumeStation(5);
-        station.nextStation();
-        station.nextVolume();
-
-        int actualStation = station.getRadioStation();
-        int actualVolume = station.getVolumeStation();
-        int actualNextStation = station.nextStation();
-        int actualNextVolume = station.nextVolume();
-
-        int expectedStation = 3;
-        int expectedVolume = 5;
-        int expectedNextStation = 4;
-        int expectedNextVolume = 6;
-
-        assertEquals(expectedStation, actualStation);
-        assertEquals(expectedVolume, actualVolume);
-        assertEquals(expectedNextStation, actualNextStation);
-        assertEquals(expectedNextVolume, actualNextVolume);
-
-    }
-
-    @Test
-    void highParamRadio() {
-        Radio station = new Radio();
-
-        station.setRadioStation(30);
-        station.setVolumeStation(40);
-        station.nextStation();
-        station.nextVolume();
-
-
-        int actualStation = station.getRadioStation();
-        int actualVolume = station.getVolumeStation();
-        int actualNextStation = station.nextStation();
-        int actualNextVolume = station.nextVolume();
-
-
-        int expectedStation = 0;
-        int expectedVolume = 0;
-        int expectedNextStation = 1;
-        int expectedNextVolume = 1;
-
-
-        assertEquals(expectedStation, actualStation);
-        assertEquals(expectedVolume, actualVolume);
-        assertEquals(expectedNextStation, actualNextStation);
-        assertEquals(expectedNextVolume, actualNextVolume);
-
-    }
-
-    @Test
-    void lowParamRadio() {
-        Radio station = new Radio();
-
-        station.setRadioStation(-9);
-        station.setVolumeStation(-10);
-        station.nextStation();
-        station.nextVolume();
-
-
-        int actualStation = station.getRadioStation();
-        int actualVolume = station.getVolumeStation();
-        int actualNextStation = station.nextStation();
-        int actualNextVolume = station.nextVolume();
-
-
-        int expectedStation = 0;
-        int expectedVolume = 0;
-        int expectedNextStation = 1;
-        int expectedNextVolume = 1;
-
-
-        assertEquals(expectedStation, actualStation);
-        assertEquals(expectedVolume, actualVolume);
-        assertEquals(expectedNextStation, actualNextStation);
-        assertEquals(expectedNextVolume, actualNextVolume);
-
-    }
-
-    @Test
-    void granLowParamRadio() {
-        Radio station = new Radio();
-
-        station.setRadioStation(0);
-        station.setVolumeStation(0);
-        station.nextStation();
-        station.nextVolume();
-
-
-        int actualStation = station.getRadioStation();
-        int actualVolume = station.getVolumeStation();
-        int actualNextStation = station.nextStation();
-        int actualNextVolume = station.nextVolume();
-
-
-        int expectedStation = 0;
-        int expectedVolume = 0;
-        int expectedNextStation = 1;
-        int expectedNextVolume = 1;
-
-
-        assertEquals(expectedStation, actualStation);
-        assertEquals(expectedVolume, actualVolume);
-        assertEquals(expectedNextStation, actualNextStation);
-        assertEquals(expectedNextVolume, actualNextVolume);
-
-    }
-
-    @Test
-    void granHighParamRadio() {
-        Radio station = new Radio();
-
-        station.setRadioStation(9);
-        station.setVolumeStation(10);
-        station.nextStation();
-        station.nextVolume();
-
-
-        int actualStation = station.getRadioStation();
-        int actualVolume = station.getVolumeStation();
-        int actualNextStation = station.nextStation();
-        int actualNextVolume = station.nextVolume();
-
-
-        int expectedStation = 9;
-        int expectedVolume = 10;
-        int expectedNextStation = 0;
-        int expectedNextVolume = 10;
-
-
-        assertEquals(expectedStation, actualStation);
-        assertEquals(expectedVolume, actualVolume);
-        assertEquals(expectedNextStation, actualNextStation);
-        assertEquals(expectedNextVolume, actualNextVolume);
-
-    }
-
-    @Test
-    void granHighParamRadioForNextPrev() {
-        Radio station = new Radio();
-
-        station.setRadioStation(8);
-        station.setVolumeStation(9);
-        station.nextStation();
-        station.nextVolume();
-
-
-        int actualStation = station.getRadioStation();
-        int actualVolume = station.getVolumeStation();
-        int actualNextStation = station.nextStation();
-        int actualNextVolume = station.nextVolume();
-
-
-        int expectedStation = 8;
-        int expectedVolume = 9;
-        int expectedNextStation = 9;
-        int expectedNextVolume = 10;
-
-
-        assertEquals(expectedStation, actualStation);
-        assertEquals(expectedVolume, actualVolume);
-        assertEquals(expectedNextStation, actualNextStation);
-        assertEquals(expectedNextVolume, actualNextVolume);
-
-    }
-
-    @Test
-    void granLowParamRadioForNextPrev() {
-        Radio station = new Radio();
-
-        station.setRadioStation(1);
-        station.setVolumeStation(1);
-        station.nextStation();
-        station.nextVolume();
-
-
-        int actualStation = station.getRadioStation();
-        int actualVolume = station.getVolumeStation();
-        int actualNextStation = station.nextStation();
-        int actualNextVolume = station.nextVolume();
-
-
-        int expectedStation = 1;
-        int expectedVolume = 1;
-        int expectedNextStation = 2;
-        int expectedNextVolume = 2;
-
-
-        assertEquals(expectedStation, actualStation);
-        assertEquals(expectedVolume, actualVolume);
-        assertEquals(expectedNextStation, actualNextStation);
-        assertEquals(expectedNextVolume, actualNextVolume);
-
-    }
-
-    @Test
-    void granLowNegativeParamRadio() {
-        Radio station = new Radio();
-
-        station.setRadioStation(-1);
-        station.setVolumeStation(-1);
-        station.nextStation();
-        station.nextVolume();
-
-
-        int actualStation = station.getRadioStation();
-        int actualVolume = station.getVolumeStation();
-        int actualNextStation = station.nextStation();
-        int actualNextVolume = station.nextVolume();
-
-
-        int expectedStation = 0;
-        int expectedVolume = 0;
-        int expectedNextStation = 1;
-        int expectedNextVolume = 1;
-
-
-        assertEquals(expectedStation, actualStation);
-        assertEquals(expectedVolume, actualVolume);
-        assertEquals(expectedNextStation, actualNextStation);
-        assertEquals(expectedNextVolume, actualNextVolume);
-
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/nextVolume.csv"})
+    public void nextParamRadioVolume(int volume, int expected) {
+        Radio next = new Radio();
+        next.nextVolume(volume);
+        int actual = next.nextVolume(volume);
+        assertEquals(expected, actual);
     }
 }
